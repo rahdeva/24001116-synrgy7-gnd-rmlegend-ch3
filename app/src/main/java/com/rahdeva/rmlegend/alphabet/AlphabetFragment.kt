ce55841a931713450c30b854128d7893c6527ed3
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-29 05:01:12 +0700

    Update AlphabetFragment.kt

--- app/src/main/java/com/rahdeva/rmlegend/alphabet/AlphabetFragment.kt
+++ app/src/main/java/com/rahdeva/rmlegend/alphabet/AlphabetFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rahdeva.rmlegend.R
 import com.rahdeva.rmlegend.databinding.FragmentAlphabetBinding
@@ -43,9 +42,9 @@ class AlphabetFragment : Fragment() {
         getParentFragmentManager().beginTransaction()
             .replace(R.id.alphabet_container, legendsFragment)
             .commit()
-        val actionToLegendFragment = AlphabetFragmentDirections
-            .actionAlphabetFragmentToLegendsFragment()
-        findNavController().navigate(actionToLegendFragment)
+//        val actionToLegendFragment = AlphabetFragmentDirections
+//            .actionAlphabetFragmentToLegendsFragment()
+//        findNavController().navigate(actionToLegendFragment)
     }
 
     companion object {
